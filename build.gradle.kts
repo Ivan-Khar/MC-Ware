db1d74f2caae82513dc1719dadd68b6689bc051c
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-02-08 13:40:39 +0300

    use kotlin's java + more properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
 plugins {
-    id("java")
+    alias(libs.plugins.kotlin.jvm)
     id("com.gradleup.shadow") version "8.3.0"
 }
+
+group = "${project.property("mc_ware_group")}"
+version = "${project.property("mc_ware_version")}"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(libs.guava)
+
     implementation("net.minestom:minestom:${project.property("minestom_version")}")
     implementation("org.apache.logging.log4j:log4j-bom:${project.property("log4j_version")}")
     implementation("org.apache.logging.log4j:log4j-api:${project.property("log4j_version")}")
@@ -17,6 +22,15 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
     }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.register("run", JavaExec::class.java) {
+    mainClass.set("one.theaq.mcware.MainKt")
+    classpath = sourceSets.main.get().runtimeClasspath
 }
 
 tasks {
