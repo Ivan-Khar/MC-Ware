b785fde1f068f2d29c0e77b9e25522af84acd382
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-02 20:59:50 +0300

    ditch custom generator(for now) world loading for instances from file

--- src/main/kotlin/one/theaq/mcware/instance/InstanceRegistry.kt
+++ src/main/kotlin/one/theaq/mcware/instance/InstanceRegistry.kt
@@ -2,20 +2,18 @@ package one.theaq.mcware.instance
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.instance.Instance
-import net.minestom.server.instance.generator.Generator
+import net.minestom.server.instance.anvil.AnvilLoader
 import net.minestom.server.registry.RegistryKey
 import net.minestom.server.world.DimensionType
 import one.theaq.mcware.dimension.DimensionRegistry
-import one.theaq.mcware.instance.generator.LobbyGenerator
 
 object InstanceRegistry {
-    val LOBBY = registerInstance(DimensionRegistry.LOBBY, LobbyGenerator())
+    val LOBBY = registerInstance(DimensionRegistry.LOBBY, "worlds/lobby")
 
-    fun registerInstance(dimensionType: RegistryKey<DimensionType>, generator: Generator): Instance {
+    fun registerInstance(dimensionType: RegistryKey<DimensionType>, worldPath: String): Instance {
         val instanceManager = MinecraftServer.getInstanceManager()
         val instance = instanceManager.createInstanceContainer(dimensionType)
-        instance.setGenerator(generator)
-
+        instance.chunkLoader = AnvilLoader(worldPath)
         return instance
     }
 }
--- src/main/kotlin/one/theaq/mcware/server/ServerEvents.kt
+++ src/main/kotlin/one/theaq/mcware/server/ServerEvents.kt
@@ -1,6 +1,7 @@
 package one.theaq.mcware.server
 
 import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.GameMode
 import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import one.theaq.mcware.instance.InstanceRegistry
@@ -13,6 +14,7 @@ class ServerEvents(val globalEventHandler: GlobalEventHandler) {
     fun asyncPlayerConfiguration(event: AsyncPlayerConfigurationEvent) {
         val player = event.player
         event.spawningInstance = InstanceRegistry.LOBBY
-        player.respawnPoint = Pos(0.0, 4.0, 0.0)
+        player.respawnPoint = Pos(0.0, 2.0, 0.0)
+        player.gameMode = GameMode.ADVENTURE
     }
 }
