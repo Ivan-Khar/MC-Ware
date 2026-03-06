8f8a9e3b5fc6788876532d79f9f8dbb2abd16115
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-06 11:56:56 +0300

    useless comments just for me and myself

--- src/main/kotlin/one/theaq/mcware/logic/GameStage.kt
+++ src/main/kotlin/one/theaq/mcware/logic/GameStage.kt
@@ -1,10 +1,14 @@
 package one.theaq.mcware.logic
 
 enum class GameStage {
+	CLEANUP, // Clean up all the variables before starting
 	WAITING_FOR_PLAYERS, // Waiting for players, duh
-	STARTING, //
-	MICRO_GAME, //
-	SPEED_INCREASE, //
-	BOSS_GAME, //
-	FINAL_SCORES //
+	STARTING, // Main initialization, should pick micro games from data folder. +may pick some game modifiers
+	MICRO_GAME, // Starts micro game from generated micro game list
+	MICRO_GAME_END, // Micro game end, check if game speed needs to increase, goes back to MICRO_GAME unless
+	SPEED_INCREASE, // Increases instance's tick speed and then goes back to MICRO_GAME
+	BOSS_GAME_INTRO, // boss game title, resets tick speed
+	BOSS_GAME, // boss game itself
+	BOSS_GAME_END, // happens when boss game finishes, should calculate all the scores for the FINAL_SCORES stage
+	FINAL_SCORES // shows final scores, goes back to CLEANUP stage after
 }
